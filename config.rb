7704343b71ba2cc6129dcbb0769355e299519a63
Guto Oliveira <contato@gutooliveira.me>
2015-03-28 17:09:02 -0300

    Instalando o SASS com COMPASS no projeto.

---
+++ config.rb
@@ -0,0 +1,18 @@
+# Require any additional compass plugins here.
+
+# Paths configuration
+css_dir = "www/css"
+sass_dir = "sass"
+
+# Output style of the compiled CSS
+output_style = (environment == :production) ? :compressed : :expanded
+
+# Prefere relative paths in the CSS
+relative_assets = true
+
+# Display the original location (SCSS file and line) of the selector
+# Useful for debugging
+line_comments = true
+
+# SCSS and not SASS
+preferred_syntax = :scss
